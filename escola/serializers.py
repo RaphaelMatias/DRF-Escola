d7131895ce4a41ce0ea6e7044a1286007ef2e58e
Raphael Matias <phael110199@gmail.com>
2024-08-23 09:43:09 -0300

    criacao da view, serializer, url e a pagina do admin das matriculas

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from escola.models import Estudante, Curso
+from escola.models import Estudante, Curso, Matricula
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,9 @@ class EstudanteSerializer(serializers.ModelSerializer):
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
+        fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
         fields = '__all__'
